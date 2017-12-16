7601fc4e0d71d51a1683fcc33e6572464153b2d4
Bao Bao <nguyenbao.95@gmail.com>
2017-12-16 20:38:14 +0700

    Add empty project

---
+++ PiChat/PiChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PiChat
+//
+//  Created by Bao Bao on 12/16/17.
+//  Copyright © 2017 baonq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PiChat/PiChat/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PiChat
+//
+//  Created by Bao Bao on 12/16/17.
+//  Copyright © 2017 baonq. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
